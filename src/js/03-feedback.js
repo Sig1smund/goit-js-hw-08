98f18369f5945900c688e8489cf8586b99f6447a
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-26 15:21:47 +0200

    task-03 complete

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import { throttle } from "lodash";
 
+const inputForm = document.querySelector(".feedback-form");
+
+inputForm.addEventListener("input", throttle(onFormInput, 500));
+inputForm.addEventListener("submit", onFormSubmit);
+
+function onFormInput() {
+  const data = {
+    email: inputForm.email.value,
+    message: inputForm.message.value,
+  };
+  localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+
+  localStorage.removeItem("feedback-form-state");
+}
+
+function fullfillForm() {
+  const filledForm = localStorage.getItem("feedback-form-state");
+  const toParseForm = JSON.parse(filledForm);
+
+  if (filledForm) {
+    inputForm.email.value = toParseForm.email;
+    inputForm.message.value = toParseForm.message;
+  }
+}
+
+fullfillForm();
