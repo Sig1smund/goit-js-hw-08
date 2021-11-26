5a04a4ff3f874c7aa54ed49caeb56febb42d551a
Sig1smund <87072894+Sig1smund@users.noreply.github.com>
2021-11-26 14:19:26 +0200

    task-02 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from "@vimeo/player";
+import { throttle } from "lodash";
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
+const onTimeUpdate = (event) => {
+  localStorage.setItem(
+    "videoplayer-current-time",
+    JSON.stringify(event.seconds)
+  );
+};
+
+player.on("timeupdate", throttle(onTimeUpdate, 1000));
